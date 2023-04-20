8684b86a36e7193f09085c881dd16bdb95af19ed
catohas <petr.hron04@gmail.com>
2023-04-20 17:36:43 +0200

    Update index.js 2

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world! ABCDEFGH');
+  res.send('Hello, test!');
+})
+
+app.get('/test', (req, res) => {
+  res.send('Testing!');
 })
 
 app.listen(port, () => {
